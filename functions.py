81032300a9cd2b75ba9ff842911068a151201be8
otto joash <louisjoshbricks@gmail.com>
2022-10-05 01:26:42 -0400

    first commit

---
+++ functions.py
@@ -0,0 +1,58 @@
+# integers
+# print(10)
+# print(2)
+# print(-2)
+
+# floats -  floating-point numbers
+# print(10.2)
+# print(2.4)
+# print(.4)
+# print(-.6)
+# print(1.0)
+
+# Octadecimal Base
+# print(0o127)
+
+# Hexadecimal
+# print(0x123)
+
+
+# print(3e8)
+# print(3e-2)
+
+
+# print(8+2)
+# print(8-2)
+# print(8*2)
+# print(8/2)
+
+# print(8 % 3)
+# print(round(8 / 3, 0))
+# print(8 // 3 * 3/ 5)
+# print(2**3)
+
+
+# print(1_000_000)
+
+
+# name
+# Bread
+# Unit Cost
+# Quantity
+# Cost
+name = input("What is your name? ")
+item = input("What item are you taking? ")
+unit_cost = float(input("What is the unit cost? "))
+quantity = float(input("What is the quantity? "))
+total_cost = quantity * unit_cost
+print(total_cost)
+print('*' * 20)
+print('Hello', name, 'here is your bill for', item)
+print('Unit cost: ', unit_cost)
+print('Total cost: ', total_cost)
+print('Thank you for shopping with BSIT Conf.')
+print('*' * 20)
+
+# our_value = float(input("Enter anything "))
+# print(our_value)
+# print(type(our_value))
