7599b96ceaef8ad1f4ddff91afadc3e5c70a21a1
otto joash <louisjoshbricks@gmail.com>
2022-10-05 02:05:33 -0400

    adding and removing elements

--- lists.py
+++ lists.py
@@ -1,3 +1,7 @@
 #my_list = [1, "mukono", "kampala", 4]
 #print(my_list[-2])
 #print(len(my_list))
+
+hostels = ["tupendane","pameja","premium","Victoria","cronnos","sabiti","david's ark"]
+
+print(hostels[0:3])
