6691361225557e7499cb8ea59ac34422d3968767
otto joash <louisjoshbricks@gmail.com>
2022-10-05 02:12:03 -0400

    range

--- lists.py
+++ lists.py
@@ -2,6 +2,10 @@
 #print(my_list[-2])
 #print(len(my_list))
 
-hostels = ["tupendane","pameja","premium","Victoria","cronnos","sabiti","david's ark"]
+#listing elements or eliminating them
+#hostels = ["tupendane","pameja","premium","Victoria","cronnos","sabiti","david's ark"]
 
-print(hostels[-1:3])
+#print(hostels[-1:3])
+
+#range function
+
