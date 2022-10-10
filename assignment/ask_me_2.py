08a79bd7ea017e10665bb20279f15a8867474051
otto joash <louisjoshbricks@gmail.com>
2022-10-10 13:21:04 -0400

    comments

--- assignment/ask_me_2.py
+++ assignment/ask_me_2.py
@@ -1,4 +1,6 @@
+# programme that validates name if no name entered it will display hello stranger
 name=str(input("hello may you enter your name please: "))
+#statement to check data entered 
 if name == "":
     print("hello, how you doing stranger")
 else:    
