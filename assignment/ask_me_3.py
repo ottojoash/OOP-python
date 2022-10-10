20fcc54d0c23b6bf1d5b91551889386e60e13cad
otto joash <louisjoshbricks@gmail.com>
2022-10-10 05:00:18 -0400

    assig

---
+++ assignment/ask_me_3.py
@@ -0,0 +1,19 @@
+name=str(input("hello may you enter your name please: "))
+if (name == name):
+    print("hello Friend!!! ",name)
+elif (name == "jackie"):
+    print("hello ",name)
+elif (name == "jack"):  
+    print("hello",name)
+else:
+    print("good bye")
+
+
+num = int(input("enter your age"))
+if num <= 18:
+    print("you are below age of working")
+elif 18<25:
+    print("look for a job")
+elif 25>=35:
+    print("you should have a job allreadly")        
+        
