76dc4f1035bc2acb6d3388ab16dae2c88d93c9bf
otto joash <louisjoshbricks@gmail.com>
2022-10-13 07:47:36 -0400

    fun

--- user_prompt.py
+++ user_prompt.py
@@ -1,11 +1,13 @@
 def user_prompt():
-    
-print("what is your name?")
-name = input()
 
-print("your name is: ",name)
+    print("what is your name?")
+    name = input()
 
-print("what is your age")
-age = input()
+    print("your name is: ",name)
+ 
+    print("what is your age")
+    age = input()
 
-print("your age is : ",age)
+    print("your age is : ",age)
+
+user_prompt    
