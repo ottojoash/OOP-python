45bd61ed00f081a1cdadf6038a19391d7e8ad1d4
otto joash <louisjoshbricks@gmail.com>
2022-10-14 02:34:29 -0400

    data

--- person.py
+++ person.py
@@ -1,10 +1,17 @@
 def main():
-    get_person()
+    person = get_person()
 
+    if person[0] == 'sandra':
+        person[1] = "UG"
+    print(
+        f"My name is {person[0]}, my age is {person[2]} and my country is {person[1]} ")
 
 
 def get_person():
-    name =input("what is your name? ")
-    country = input("what is your country? ")
-    print(f"name is {name} country {country}")    
-main()        
+    name = input("What is your name? ")
+    country = input("What is your country? ")
+    age = input("What is your age? ")
+    return (name, country, age)
+
+
+main()
