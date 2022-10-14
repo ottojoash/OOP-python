85c4f7ae3fe49eaef83ec0fa099d91a3c461915f
otto joash <louisjoshbricks@gmail.com>
2022-10-14 02:01:23 -0400

    data

--- person.py
+++ person.py
@@ -6,5 +6,5 @@ def main():
 def get_person():
     name =input("what is your name? ")
     country = input("what is your country? ")
-        
+    print(f"name is {name} country {country}")    
 main()        
