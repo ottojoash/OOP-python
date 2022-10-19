2266e5406aba2ee7baba9fbfefe266d7bde74b61
otto joash <louisjoshbricks@gmail.com>
2022-10-19 01:52:47 -0400

    changes

--- assignment2/loop1.py
+++ assignment2/loop1.py
@@ -1,4 +1,4 @@
 for i in range(0, 11):
-
+#if statement to get the odd numbers by diving by two with no reminder
      if i % 2 != 0:
         print(i)
---
+++ assignment2/number7.py
@@ -0,0 +1,4 @@
+def entry(name="",birth_year="",weight=""):
+    print(input("enter your name"))
+    print(input("enter your birth_year"))
+    print(input("enter your weight"))
