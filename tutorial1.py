5d610ed41d91189b5ff4705ea30474d275c7b857
ottojoash <louisjoshbricks@gmail.com>
2022-10-20 09:09:43 -0400

    comment

--- tutorial1.py
+++ tutorial1.py
@@ -4,7 +4,7 @@ from unicodedata import name
 class item:
     #adding behavioural to objects
 
-    def __init__(self):
+    def __init__(self,name,quantity,price):
         self.name = name
         self.quantity = quantity
         self.price = price
@@ -16,21 +16,21 @@ class item:
         return x*y
 
 car = item("telsa",100,5000)
-gun = item("")
+gun = item("m16", 400, 1000)
 
-phone = item()
-phone.name = "itel6"
-phone.version = "andriod 12"
-phone.price = 2000
+#phone = item()
+# phone.name = "itel6"
+# phone.version = "andriod 12"
+# phone.price = 2000
 
-earphones = item()
-earphones.price = 1000
+# earphones = item()
+# earphones.price = 1000
 
 #calling methods of class
 
-print(phone.calculate_discount(0.8,phone.price))
-print(earphones.calculate_discount(0.5,earphones.price))
+# print(phone.calculate_discount(0.8,phone.price))
+# print(earphones.calculate_discount(0.5,earphones.price))
 
 #print type of phone
-print(phone.name)
-print(phone.price)
+# print(phone.name)
+# print(phone.price)
