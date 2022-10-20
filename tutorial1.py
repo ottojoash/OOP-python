80afb359c6fd4589c7e732ae9a8730d4ee719bcd
ottojoash <louisjoshbricks@gmail.com>
2022-10-20 08:16:58 -0400

    correct

--- tutorial1.py
+++ tutorial1.py
@@ -11,6 +11,14 @@ phone.name = "itel6"
 phone.version = "andriod 12"
 phone.price = 2000
 
+earphones = item()
+earphones.price = 1000
+
+#calling methods of class
+
+print(phone.calculate_discount(0.8,phone.price))
+print(earphones.calculate_discount(0.5,earphones.price))
+
 #print type of phone
 print(phone.name)
 print(phone.price)
