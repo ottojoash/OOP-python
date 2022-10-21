c3384f13b3cd4a0c54adb83e66237b69bc1fcb2d
ottojoash <louisjoshbricks@gmail.com>
2022-10-21 02:24:30 -0400

    data

--- inheritance.py
+++ inheritance.py
@@ -6,7 +6,14 @@ class person:
     def __str__(self):
         return f"the name is {self.name}"
           
+class  student(person):
+    def __init__(self, name, age,access_no,program):
+        super().__init__(name, age)
+        self.access_no = access_no
+        self.program = program
 
+student1 = student('jane doe', 16 ,'a0006','BBA')
+print(student)        
 person1 = person('john',70)
 the_second_person = person('jane doe',15)
 
