30649cecab00c33779706a3173b7fcdf8c05eb17
ottojoash <louisjoshbricks@gmail.com>
2022-10-26 02:05:31 -0400

    week8

---
+++ week8/codeaccessmodifiers.py
@@ -0,0 +1,17 @@
+from shutil import register_unpack_format
+
+
+class Person:
+    def __init__(self,name,age,country):
+        if(country not in ['uganda','kenya']):
+            raise('country not found')
+        self.name = name
+        self.age = age
+        self.country = country
+
+    def __str__(self):
+        return f'name is{self.name} and age is {self.age} and country is {self.country}'
+
+person1 = Person('john doe', 16)
+person1.name = 'jimmy'
+print(person1)
