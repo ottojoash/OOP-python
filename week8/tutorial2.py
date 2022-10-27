f384c7331e357ba6700a00691fbcd2972e53b973
ottojoash <louisjoshbricks@gmail.com>
2022-10-27 08:11:15 -0400

    anothr

--- week8/tutorial2.py
+++ week8/tutorial2.py
@@ -12,3 +12,18 @@ print('joan')
 
 age  = 10
 print(age)
+
+class Student:
+
+    def eat(self):
+        print("i am eating")
+
+    def drink(self):
+        print('iam drinking')
+
+    def sleep(self):
+        print('iam sleeping')
+
+    def wake(self):
+        print('iam waking')            
+        
